53412523db5ac3a96c622d35831a7778f2186613
VallyL <levin.valeriia01@gmail.com>
2024-08-11 17:20:20 +0200

    push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Rating from "./components/Rating";
+import GuestList from "./components/List";
+
+function App() {
+  return (
+    <>
+      <Rating />
+      <GuestList />
+    </>
+  );
+}
+
+export default App;
